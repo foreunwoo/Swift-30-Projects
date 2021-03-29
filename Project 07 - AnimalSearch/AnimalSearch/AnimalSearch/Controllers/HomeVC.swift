3a1ad890471445363c71663bb6afded3fec952b7
foreunwoo <agasiya99@gmail.com>
2021-03-29 23:21:25 +0900

    [UPDATE] scope 버튼 삭제, 검색 기능 구현

--- Project 07 - AnimalSearch/AnimalSearch/AnimalSearch/Controllers/HomeVC.swift
+++ Project 07 - AnimalSearch/AnimalSearch/AnimalSearch/Controllers/HomeVC.swift
@@ -14,45 +14,43 @@ class HomeVC: UIViewController {
     let searchController = UISearchController(searchResultsController: nil)
     
     let animalsTableView = UITableView()
+        
+    var animalList = [Animal]()
+    var filteredList = [Animal]()
     
-    let animalList = [["고양이", "포유류"], ["뱀", "파충류"],
-                      ["개", "포유류"], ["물수리", "조류"],
-                      ["염소", "포유류"], ["딱따구리", "조류"],
-                      ["말", "포유류"], ["딱새", "조류"],
-                      ["원숭이", "포유류"], ["청둥오리", "조류"],
-                      ["코끼리", "포유류"], ["왜가리", "조류"],
-                      ["기린", "포유류"], ["타조", "조류"],
-                      ["개구리", "양서류"],["도룡뇽", "양서류"],
-                      ["두꺼비", "양서류"], ["맹꽁이", "양서류"],
-                      ["도마뱀", "파충류"], ["거북이", "파충류"],
-                      ["악어", "파충류"],]
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-
+        
         setSearchController()
         setTableView()
+        
+        setData()
     }
     
     func setSearchController() {
         searchController.searchBar.placeholder = "Search animals"
-        searchController.searchBar.scopeButtonTitles = [ "포유류", "양서류", "파충류", "조류" ]
+        searchController.searchResultsUpdater = self
         
         self.navigationItem.searchController = searchController
         self.navigationItem.title = "Animals"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.hidesSearchBarWhenScrolling = true
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     func setTableView() {
         _ = animalsTableView.then {
             $0.delegate = self
             $0.dataSource = self
-            
-            $0.register(AnimalsTVCell.self, forCellReuseIdentifier: "AnimalsTVCell")
-            
+                        
             view.addSubview($0)
             
             $0.snp.makeConstraints {
@@ -62,6 +60,33 @@ class HomeVC: UIViewController {
                 $0.bottom.equalTo(view.snp.bottom)
             }
         }
+    }
+    
+    func setData() {
+        animalList.append(Animal("고양이", .a))
+        animalList.append(Animal("개", .a))
+        animalList.append(Animal("염소", .a))
+        animalList.append(Animal("말", .a))
+        animalList.append(Animal("원숭이", .a))
+        animalList.append(Animal("코끼리", .a))
+        animalList.append(Animal("기린", .a))
+        
+        animalList.append(Animal("개구리", .b))
+        animalList.append(Animal("두꺼비", .b))
+        animalList.append(Animal("도룡뇽", .b))
+        animalList.append(Animal("맹꽁이", .b))
+        
+        animalList.append(Animal("뱀", .c))
+        animalList.append(Animal("도마뱀", .c))
+        animalList.append(Animal("거북이", .c))
+        animalList.append(Animal("악어", .c))
+        
+        animalList.append(Animal("물수리", .d))
+        animalList.append(Animal("딱따구리", .d))
+        animalList.append(Animal("딱새", .d))
+        animalList.append(Animal("청둥오리", .d))
+        animalList.append(Animal("왜가리", .d))
+        animalList.append(Animal("타조", .d))
     }
 
 }
@@ -77,18 +102,31 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return animalList.count
+        return isFiltering ? filteredList.count : animalList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalsTVCell", for: indexPath) as! AnimalsTVCell
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cell")
         cell.selectionStyle = .none
         
-        cell.animal = Animal(animalList[indexPath.row][0], animalList[indexPath.row][1])
-        cell.fillDataToView()
+        if isFiltering {
+            cell.textLabel?.text = filteredList[indexPath.row].name
+            cell.detailTextLabel?.text = filteredList[indexPath.row].type.rawValue
+        } else {
+            cell.textLabel?.text = animalList[indexPath.row].name
+            cell.detailTextLabel?.text = animalList[indexPath.row].type.rawValue
+        }
         
         return cell
     }
-    
-    
+}
+
+extension HomeVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredList = animalList.filter({ (animal:Animal) -> Bool in
+            return animal.name.lowercased().contains(searchController.searchBar.text!.lowercased())
+        })
+        
+        self.animalsTableView.reloadData()
+    }
 }
--- Project 07 - AnimalSearch/AnimalSearch/AnimalSearch/Model/Animal.swift
+++ Project 07 - AnimalSearch/AnimalSearch/AnimalSearch/Model/Animal.swift
@@ -8,11 +8,18 @@
 import Foundation
 
 struct Animal {
-    let name: String?
-    let type: String?
+    let name: String
+    let type: Type
     
-    init(_ name: String, _ type: String) {
+    init(_ name: String, _ type: Type) {
         self.name = name
         self.type = type
     }
+}
+
+enum Type: String {
+    case a = "포유류"
+    case b = "양서류"
+    case c = "파충류"
+    case d = "조류"
 }
