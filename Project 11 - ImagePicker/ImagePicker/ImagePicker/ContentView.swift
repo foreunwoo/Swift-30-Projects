0de6b978c8baf03fe084e9eafeff952b217936df
foreunwoo <agasiya99@gmail.com>
2021-04-25 20:43:10 +0900

    init project

---
+++ Project 11 - ImagePicker/ImagePicker/ImagePicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePicker
+//
+//  Created by eunwoo on 2021/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
